b2589c3e1ba6e667e6667cf4d7c467e6b12872ef
marianosim <mariano.simantob@gmail.com>
2023-01-17 18:15:04 -0300

    useMemo Hook

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MemoHook } from './06-memos/MemoHook';
+//import { Memorize } from './06-memos/Memorize';
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 //import { MultipleCustomHooks } from './03-exaples/MultipleCustomHooks';
 //import { FocusScreen } from './04-useRef/FocusScreen';
-import { Layout } from './05-useLayoutEffect/Layout';
+//import { Layout } from './05-useLayoutEffect/Layout';
 //import { CounterApp } from './01-useState/CounterApp';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 //import { SimpleForm } from './02-useEffect/SimpleForm';
@@ -13,7 +15,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Layout />
+    <MemoHook />
   </React.StrictMode>
 );
 
